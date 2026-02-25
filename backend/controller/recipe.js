7e855b1a705c6a6120500db7d5002fa867c757c9
aliya mahaboob <mahaboobaliya02@gmail.com>
2026-02-25 18:01:01 +0530

    backend api and frontend setup

--- backend/controller/recipe.js
+++ backend/controller/recipe.js
@@ -1,24 +1,48 @@
-const getRecipes=async(req,res)=>{
-    res.json({message:"Get all recipes"})
+const Recipes = require("../models/recipe")
+
+const getRecipes = async (req, res) => {
+    const recipes = await Recipes.find()
+    return res.json(recipes)
 }
 
-const getRecipe=async(req,res)=>{
-    res.json({message:"Get recipe by id"})
+const getRecipe = async (req, res) => {
+    const recipe = await Recipes.findById(req.params.id)
+    res.json(recipe)
 }
 
-const addRecipe = async(req,res)=>{
-    res.json({message:"Add recipe"})
+const addRecipe = async (req, res) => {
+    const { title, ingredients, instructions, time } = req.body
+
+    if (!title || !ingredients || !instructions) {
+        res.json({ message: "Required fields can't be empty" })
+    }
+
+    const newRecipe = await Recipes.create({
+        title, ingredients, instructions, time
+    })
+    return res.json(newRecipe)
 }
 
-const editRecipe = async(req,res)=>{
-    res.json({message:"Edit recipe"})
+const editRecipe = async (req, res) => {
+    const { title, ingredients, instructions, time } = req.body
+    let recipe = await Recipes.findById(req.params.id)
+
+    try {
+        if (recipe) {
+            await Recipes.findByIdAndUpdate(req.params.id, req.body, { new: true })
+            res.json({ title, ingredients, instructions, time })
+        }
+    }
+    catch (err) {
+        return res.status(404).json({ message: err })
+    }
 }
 
-const deleteRecipe = async(req,res)=>{
-    res.json({message:"Delete recipe"})
+const deleteRecipe = async (req, res) => {
+    res.json({ message: "Delete recipe" })
 }
 
 
 
-module.exports = {getRecipes, getRecipe, addRecipe, editRecipe, deleteRecipe}
+module.exports = { getRecipes, getRecipe, addRecipe, editRecipe, deleteRecipe }
 
